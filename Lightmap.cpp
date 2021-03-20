c820a0b38709533213ee8e67cfce977d30917a7f
shadowstormone <shadowstormone@mail.ru>
2021-03-20 18:20:43 +0500

    Добавьте файлы проекта.

---
+++ Lightmap.cpp
@@ -0,0 +1,13 @@
+#include "Lightmap.h"
+#include "Chunk.h"
+
+Lightmap::Lightmap(){
+	map = new unsigned short[CHUNK_VOL];
+	for (unsigned int i = 0; i < CHUNK_VOL; i++){
+		map[i] = 0x0000;
+	}
+}
+
+Lightmap::~Lightmap(){
+	delete[] map;
+}
